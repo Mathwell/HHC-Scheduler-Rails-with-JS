1d2842bfb27ae1401f2375912c64e8b928e4a314
Mathwell <eageeva@hotmail.com>
2018-12-22 20:42:35 -0800

    ES6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,10 +25,14 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
+  # with
+  config.assets.js_compressor = Uglifier.new(harmony: true)
+
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
